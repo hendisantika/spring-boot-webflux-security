0cafef631bcfa7a851a9d82d480783c182f84831
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:39:10 +0700

    Add Authority entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxsecurity/entity/Authority.java
@@ -0,0 +1,58 @@
+package id.my.hendisantika.webfluxsecurity.entity;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/12/24
+ * Time: 11.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Document(collection = "authority")
+public class Authority implements Serializable, GrantedAuthority {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Size(max = 50)
+    @Id
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Authority authority = (Authority) o;
+
+        return !(!Objects.equals(name, authority.name));
+    }
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
