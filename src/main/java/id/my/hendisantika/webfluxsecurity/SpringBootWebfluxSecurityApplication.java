f060ceeab517c98402795418a317b964ac4748fa
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:41:18 +0700

    Add @EnableWebFlux n @EnableReactiveMongoRepositories🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxsecurity/SpringBootWebfluxSecurityApplication.java
+++ src/main/java/id/my/hendisantika/webfluxsecurity/SpringBootWebfluxSecurityApplication.java
@@ -2,8 +2,12 @@ package id.my.hendisantika.webfluxsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
+@EnableReactiveMongoRepositories
 public class SpringBootWebfluxSecurityApplication {
 
     public static void main(String[] args) {
