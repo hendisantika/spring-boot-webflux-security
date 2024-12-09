86a4f0c18b3b9d372f7cbd27e2503cdc3a183e67
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:46:00 +0700

    Add findByUsername in ReactiveUserDetailsServiceImpl🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxsecurity/security/ReactiveUserDetailsServiceImpl.java
+++ src/main/java/id/my/hendisantika/webfluxsecurity/security/ReactiveUserDetailsServiceImpl.java
@@ -2,8 +2,13 @@ package id.my.hendisantika.webfluxsecurity.security;
 
 import id.my.hendisantika.webfluxsecurity.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +28,13 @@ public class ReactiveUserDetailsServiceImpl implements ReactiveUserDetailsServic
 
     public ReactiveUserDetailsServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Override
+    public Mono<UserDetails> findByUsername(String login) {
+        return userRepository.findByLogin(login)
+                .filter(Objects::nonNull)
+                .switchIfEmpty(Mono.error(new BadCredentialsException(String.format("User %s not found in database", login))))
+                .map(this::createSpringSecurityUser);
     }
 }
