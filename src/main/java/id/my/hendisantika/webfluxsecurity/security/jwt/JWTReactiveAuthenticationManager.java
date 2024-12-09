bbab4efa2bfaa4d2b48082d747c5d0d7b76ffdd0
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:50:07 +0700

    Add authenticate in JWTReactiveAuthenticationManager🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxsecurity/security/jwt/JWTReactiveAuthenticationManager.java
+++ src/main/java/id/my/hendisantika/webfluxsecurity/security/jwt/JWTReactiveAuthenticationManager.java
@@ -3,8 +3,12 @@ package id.my.hendisantika.webfluxsecurity.security.jwt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 /**
  * Created by IntelliJ IDEA.
@@ -28,4 +32,19 @@ public class JWTReactiveAuthenticationManager implements ReactiveAuthenticationM
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Override
+    public Mono<Authentication> authenticate(final Authentication authentication) {
+        if (authentication.isAuthenticated()) {
+            return Mono.just(authentication);
+        }
+        return Mono.just(authentication)
+                .switchIfEmpty(Mono.defer(this::raiseBadCredentials))
+                .cast(UsernamePasswordAuthenticationToken.class)
+                .flatMap(this::authenticateToken)
+                .publishOn(Schedulers.parallel())
+                .onErrorResume(e -> raiseBadCredentials())
+                .filter(u -> passwordEncoder.matches((String) authentication.getCredentials(), u.getPassword()))
+                .switchIfEmpty(Mono.defer(this::raiseBadCredentials))
+                .map(u -> new UsernamePasswordAuthenticationToken(authentication.getPrincipal(), authentication.getCredentials(), u.getAuthorities()));
+    }
 }
