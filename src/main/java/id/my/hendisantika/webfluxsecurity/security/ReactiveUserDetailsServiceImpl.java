4242acb540565b990781efcb60a604c01dbc6322
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:46:50 +0700

    Add createSpringSecurityUser in ReactiveUserDetailsServiceImpl🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxsecurity/security/ReactiveUserDetailsServiceImpl.java
+++ src/main/java/id/my/hendisantika/webfluxsecurity/security/ReactiveUserDetailsServiceImpl.java
@@ -1,14 +1,19 @@
 package id.my.hendisantika.webfluxsecurity.security;
 
+import id.my.hendisantika.webfluxsecurity.entity.User;
 import id.my.hendisantika.webfluxsecurity.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +41,14 @@ public class ReactiveUserDetailsServiceImpl implements ReactiveUserDetailsServic
                 .filter(Objects::nonNull)
                 .switchIfEmpty(Mono.error(new BadCredentialsException(String.format("User %s not found in database", login))))
                 .map(this::createSpringSecurityUser);
+    }
+
+    private org.springframework.security.core.userdetails.User createSpringSecurityUser(User user) {
+        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getName()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(user.getLogin(),
+                user.getPassword(),
+                grantedAuthorities);
     }
 }
