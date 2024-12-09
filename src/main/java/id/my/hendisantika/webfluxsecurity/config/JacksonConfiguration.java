3e4dbfc099346f91d30f3bf959245f94aea6c257
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:37:53 +0700

    Add JacksonConfiguration🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxsecurity/config/JacksonConfiguration.java
@@ -0,0 +1,63 @@
+package id.my.hendisantika.webfluxsecurity.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/12/24
+ * Time: 11.37
+ * To change this template use File | Settings | File Templates.
+ */
+public class JacksonConfiguration {
+    @Bean
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+
+        return new Jackson2ObjectMapperBuilder()
+                .serializationInclusion(JsonInclude.Include.NON_EMPTY)
+                .featuresToDisable(SerializationFeature.WRITE_NULL_MAP_VALUES)
+                .serializerByType(LocalDateTime.class, new LocalDateTimeSerializer())
+                .serializerByType(LocalDate.class, new LocalDateSerializer())
+                .deserializerByType(LocalDate.class, new LocalDateDeserializer())
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer());
+    }
+
+    @Bean
+    Jackson2JsonEncoder jackson2JsonEncoder(ObjectMapper mapper) {
+        return new Jackson2JsonEncoder(mapper);
+    }
+
+    @Bean
+    Jackson2JsonDecoder jackson2JsonDecoder(ObjectMapper mapper) {
+        return new Jackson2JsonDecoder(mapper);
+    }
+
+    @Bean
+    WebFluxConfigurer webFluxConfigurer(Jackson2JsonEncoder encoder, Jackson2JsonDecoder decoder) {
+        return new WebFluxConfigurer() {
+            @Override
+            public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+                configurer.defaultCodecs().jackson2JsonEncoder(encoder);
+                configurer.defaultCodecs().jackson2JsonDecoder(decoder);
+            }
+        };
+    }
+}
