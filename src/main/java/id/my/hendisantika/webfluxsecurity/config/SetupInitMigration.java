e9351f84d06dc54bf140d1a7ffcf54c4ad84ef09
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:53:10 +0700

    Add addAuthorities in SetupInitMigration🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxsecurity/config/SetupInitMigration.java
+++ src/main/java/id/my/hendisantika/webfluxsecurity/config/SetupInitMigration.java
@@ -1,6 +1,10 @@
 package id.my.hendisantika.webfluxsecurity.config;
 
 import com.github.mongobee.changeset.ChangeLog;
+import com.github.mongobee.changeset.ChangeSet;
+import id.my.hendisantika.webfluxsecurity.entity.AuthoritiesConstants;
+import id.my.hendisantika.webfluxsecurity.entity.Authority;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +18,13 @@ import com.github.mongobee.changeset.ChangeLog;
  */
 @ChangeLog(order = "001")
 public class SetupInitMigration {
+    @ChangeSet(order = "01", author = "initiator", id = "01-addAuthorities")
+    public void addAuthorities(MongoTemplate mongoTemplate) {
+        Authority adminAuthority = new Authority();
+        adminAuthority.setName(AuthoritiesConstants.ADMIN);
+        Authority userAuthority = new Authority();
+        userAuthority.setName(AuthoritiesConstants.USER);
+        mongoTemplate.save(adminAuthority);
+        mongoTemplate.save(userAuthority);
+    }
 }
