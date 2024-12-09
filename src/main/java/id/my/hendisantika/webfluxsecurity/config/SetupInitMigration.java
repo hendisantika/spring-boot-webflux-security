ce4c5dca1ba91ee905b09ee9b9d2f6606d5603f4
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:52:58 +0700

    Add SetupInitMigration🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxsecurity/config/SetupInitMigration.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.webfluxsecurity.config;
+
+import com.github.mongobee.changeset.ChangeLog;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/12/24
+ * Time: 11.52
+ * To change this template use File | Settings | File Templates.
+ */
+@ChangeLog(order = "001")
+public class SetupInitMigration {
+}
