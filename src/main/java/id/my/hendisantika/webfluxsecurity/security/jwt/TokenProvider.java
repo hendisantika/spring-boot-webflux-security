9c6a49c9597c6ae75add4b23d859c20c17b11ae2
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:48:29 +0700

    Add getAuthentication in TokenProvider🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/webfluxsecurity/security/jwt/TokenProvider.java
+++ src/main/java/id/my/hendisantika/webfluxsecurity/security/jwt/TokenProvider.java
@@ -1,18 +1,27 @@
 package id.my.hendisantika.webfluxsecurity.security.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
+
+import static io.netty.handler.codec.http.HttpHeaderValidationUtil.validateToken;
 
 /**
  * Created by IntelliJ IDEA.
@@ -58,5 +67,24 @@ public class TokenProvider {
                 .signWith(SignatureAlgorithm.HS512, secretKey)
                 .setExpiration(validity)
                 .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        if (StringUtils.isEmpty(token) || !validateToken(token)) {
+            throw new BadCredentialsException("Invalid token");
+        }
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        User principal = new User(claims.getSubject(), "", authorities);
+
+        return new UsernamePasswordAuthenticationToken(principal, token, authorities);
     }
 }
