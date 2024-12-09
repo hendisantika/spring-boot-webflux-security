d40411c14e4f055f1b6d66f0f3a792efbe6c5beb
hendisantika <hendisantika@yahoo.co.id>
2024-12-09 11:43:11 +0700

    Add AuthoritiesConstants🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/webfluxsecurity/entity/AuthoritiesConstants.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.webfluxsecurity.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-webflux-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/12/24
+ * Time: 11.42
+ * To change this template use File | Settings | File Templates.
+ */
+public final class AuthoritiesConstants {
+
+    public static final String ADMIN = "ROLE_ADMIN";
+
+    public static final String USER = "ROLE_USER";
+
+    public static final String ANONYMOUS = "ROLE_ANONYMOUS";
+
+    private AuthoritiesConstants() {
+    }
+}
